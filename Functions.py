43fbcff89c41d29d202e93591e7dfb8c838268dc
freena <freena22@gmail.com>
2018-05-15 09:42:10 -0700

    init functions

---
+++ Functions.py
@@ -0,0 +1,19 @@
+# Functions
+
+######## 1. Defining Functions #######
+
+
+
+######## 2. Variable Scope #######
+
+
+
+######## 3. Documentation #######
+
+
+
+######## 4. Lambda Expressions #######
+
+
+
+######## 5. Iterators and Generators #######
