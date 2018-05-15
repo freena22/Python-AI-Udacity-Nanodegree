f7d7e57323dff81dc7878b971f6be52008c1d95e
freena <freena22@gmail.com>
2018-05-15 00:38:23 -0700

    List Comprehensions

--- Control_Flow_Practice.py
+++ Control_Flow_Practice.py
@@ -181,5 +181,38 @@ print(cast)
 
 # 7. List Comprehensions
 
+capitalized_cities = [city.title() for city in cities]
 
+# equal to:
+capitalized_cities = []
+for city in cities:
+    capitalized_cities.append(city.title())
+
+squares = [x**2 for x in range(9) if x % 2 == 0]
+squares = [x**2 for x in range(9) if x % 2 == 0 else x + 3
+squares = [x**2 if x % 2 == 0 else x + 3 for x in range(9)]
+
+########## Practice 1 Extract First Name ############ 
+
+names = ["Rick Sanchez", "Morty Smith", "Summer Smith", "Jerry Smith", "Beth Smith"]
+
+first_names = [name.split()[0].lower() for name in names]
+print(first_names)
+
+########## Practice 2 Multiple of Three ############ 
+
+multiples_3 = [x * 3 for x in range(1, 21)]
+print(multiples_3)
+
+########## Practice 3 Filter Names by Scores ############ 
+scores = {
+             "Rick Sanchez": 70,
+             "Morty Smith": 35,
+             "Summer Smith": 82,
+             "Jerry Smith": 23,
+             "Beth Smith": 98
+          }
+
+passed = [name for name, score in scores.items() if score >= 65]
+print(passed)
 
