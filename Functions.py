4ba64090e29bfe29f0b1143d6c12d6bdcaaeea49
freena <freena22@gmail.com>
2018-05-15 11:43:24 -0700

    iterators

--- Functions.py
+++ Functions.py
@@ -69,5 +69,38 @@ print(short_cities)
 
 # Output: ['Chicago', 'Denver', 'Boston']
 
-
 ######## 5. Iterators and Generators #######
+"""
+Iterables are objects that can return one of their elements at a time, such as a list. 
+Many of the built-in functions we’ve used so far, like 'enumerate,' return an iterator.
+An iterator is an object that represents a stream of data. 
+This is different from a list, which is also an iterable, but not an iterator because it is not a stream of data.
+
+Why Generators?
+
+Generators are a lazy way to build iterables. They are useful when the fully realized list would not fit in memory,
+or when the cost to calculate each list element is high and you want to do it as late as possible. 
+But they can only be iterated over once.
+"""
+
+lessons = ["Why Python Programming", "Data Types and Operators", "Control Flow", "Functions", "Scripting"]
+
+def my_enumerate(iterable, start=0):
+    count = start
+    for element in iterable:
+        yield count, element
+        count += 1
+
+for i, lesson in my_enumerate(lessons, 1):
+    print("Lesson {}: {}".format(i, lesson))
+
+# Output:
+"""
+Lesson 1: Why Python Programming
+Lesson 2: Data Types and Operators
+Lesson 3: Control Flow
+Lesson 4: Functions
+Lesson 5: Scripting
+"""
+
+
