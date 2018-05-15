b81107c03d5b4c935c39bb194c42c25d3fc22b56
freena <freena22@gmail.com>
2018-05-14 23:43:42 -0700

    break in the loop

--- Control_Flow_Practice.py
+++ Control_Flow_Practice.py
@@ -75,4 +75,21 @@ nearest_square = num**2
 
 print(nearest_square)
 
+# 4. Break and Continue
+
+### Write a loop with a break statement to create a string, news_ticker, that is exactly 140 characters long. 
+headlines = ["Local Bear Eaten by Man",
+             "Legislature Announces New Laws",
+             "Peasant Discovers Violence Inherent in System",
+             "Cat Rescues Fireman Stuck in Tree",
+             "Brave Knight Runs Away",
+             "Papperbok Review: Totally Triffic"]
+
+news_ticker = ''
+for healine in headlines:
+  news_ticker += headline + ''
+  if len(news_ticker) >=140:
+    news_ticker = news_ticker[:140]
+    break
+print(news_ticker)
 
